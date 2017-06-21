ff790571758aa0acffb66bfdd18ca1edf9d773f8
pkh1162 <paulyk1162@hotmail.co.uk>
2017-06-21 23:07:21 +0100

    test merge branch into master, also modified App to be container

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {connect} from "react-redux";
 //import RaisedButton from 'material-ui/RaisedButton';
 
 import '../styles/App.css';
@@ -8,12 +9,23 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-      <div className="App">
-          <h2>Aaayumi and Paul's Project!</h2>       
-      </div>
+        <div className="App">
+            <h2>Aaayumi and Paul's Project!</h2>       
+        </div>
       </MuiThemeProvider>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+  }
+}
+
+export default connect(null, null)(App);
